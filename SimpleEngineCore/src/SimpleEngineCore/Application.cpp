5953ac7474f967692abe9d0fff250c610cd76d66
Alexander <simple.coding.channel@gmail.com>
2021-05-16 16:15:12 +0200

    Added GLAD library

--- SimpleEngineCore/src/SimpleEngineCore/Application.cpp
+++ SimpleEngineCore/src/SimpleEngineCore/Application.cpp
@@ -1,6 +1,7 @@
 #include "SimpleEngineCore/Application.hpp"
 #include "SimpleEngineCore/Log.hpp"
 
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 
@@ -45,11 +46,20 @@ namespace SimpleEngine {
         /* Make the window's context current */
         glfwMakeContextCurrent(window);
 
+
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        {
+            LOG_CRITICAL("Failed to initialize GLAD");
+            return -1;
+        }
+
+        glClearColor(1, 0, 0, 0);
+
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
-            //glClear(GL_COLOR_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT);
 
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
